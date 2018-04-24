5000e59bd3aacea65614a3effe424098c20ce298
jordicros <36449492+jordicros@users.noreply.github.com>
2018-04-24 16:11:48 +0200

    Registre i consultar usuari implementats i funcionant.

--- src/main/java/JOC/Mains_test/Main.java
+++ src/main/java/JOC/Mains_test/Main.java
@@ -3,6 +3,7 @@ package JOC.Mains_test;
 import JOC.Mon.*;
 
 import okhttp3.OkHttpClient;
+import okhttp3.ResponseBody;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Call;
 import retrofit2.Response;
@@ -42,6 +43,8 @@ public class Main {
 
         // Create a call instance for looking up Retrofit contributors.
         Call<Escena> call = servei.escenas("2");
+        Call<Usuario> call2 = servei.regUsuario(new Usuario("Jordi","noob",1));
+        Call<Usuario> call3 = servei.consultarUsuarioJSON("Jordi");
 
         // Fetch and print a list of the contributors to the library.
         Response resposta= call.execute();
@@ -51,10 +54,34 @@ public class Main {
             pintar(escena);
         }
         else if(codi==204){
-            System.out.printf("No hi ha cap escena amb aquell identificador");
+            System.out.printf("No hi ha cap escena amb aquell identificador\n");
         }
         else{
-            System.out.printf("Error desconegut");
+            System.out.printf("Error desconegut\n");
+        }
+        resposta = call2.execute();
+        codi= resposta.code();
+        if(codi==200) {
+            Usuario user = (Usuario)resposta.body();
+            System.out.printf(user.getNickname()+"\n");
+        }
+        else if(codi==204){
+            System.out.printf("No hi ha cap usuari amb aquest identificador\n");
+        }
+        else{
+            System.out.printf("Error desconegut\n");
+        }
+        resposta = call3.execute();
+        codi= resposta.code();
+        if(codi==200) {
+            Usuario user = (Usuario) resposta.body();
+            System.out.printf(user.getNickname()+"\n");
+        }
+        else if(codi==204){
+            System.out.printf("No hi ha cap usuari amb aquest identificador\n");
+        }
+        else{
+            System.out.printf("Error desconegut\n");
         }
     }
 }
--- src/main/java/JOC/Mains_test/ServeiRetrofit.java
+++ src/main/java/JOC/Mains_test/ServeiRetrofit.java
@@ -1,9 +1,12 @@
 package JOC.Mains_test;
 
 import JOC.Mon.Escena;
+import JOC.Mon.Usuario;
+import JOC.Objectes.Objeto;
+import okhttp3.ResponseBody;
 import retrofit2.Call;
-import retrofit2.http.GET;
-import retrofit2.http.Path;
+import retrofit2.Response;
+import retrofit2.http.*;
 
 import java.util.List;
 
@@ -12,4 +15,13 @@ public interface ServeiRetrofit {
     @GET("/myapp/service/escenaris/{id}")
     Call<Escena> escenas(
             @Path("id") String id);
+
+    @POST("/myapp/service/usuari/register/")
+    Call<Usuario> regUsuario(@Body Usuario user);
+
+    @GET("/myapp/service/usuari/{nombre}")
+    Call<Usuario> consultarUsuarioJSON(@Path("nombre") String nombre);
+
+    @POST("/myapp/service/objecte/add/{nombre}")
+    Call<Response> newObjeto(@Path("nombre") String nombre);
 }
